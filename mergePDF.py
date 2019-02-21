a21fa1cc225a7a61a014c1a46cd5be1951a5406c
a-capra <andrea.capra85@gmail.com>
2019-02-21 20:58:20 +0100

    new version from agana

--- mergePDF.py
+++ mergePDF.py
@@ -1,29 +1,30 @@
 #!/usr/bin/env python3
+
 import PyPDF2
 import sys
 import os.path as op
 
 if __name__=='__main__':
 
-	pdfWriter = PyPDF2.PdfFileWriter()
-	argc=len(sys.argv)
-	#print('argc =',argc)
-	if argc<4:
-		print('Usage:\nmergePDF.py output.pdf input1.pdf input2.pdf ...')
-	for fpdf in sys.argv[2:argc]:
-		if not op.isfile(fpdf):	
-			print(fpdf,' does not exist... skipping')
-			continue
-		f = open(fpdf, 'rb')
-		print('-->',fpdf)
-		pdfReader = PyPDF2.PdfFileReader(f)
-		print('pages:',pdfReader.numPages)
-		for pageNum in range(pdfReader.numPages):
-			pageObj = pdfReader.getPage(pageNum)
-			pdfWriter.addPage(pageObj)
+        pdfWriter = PyPDF2.PdfFileWriter()
+        argc=len(sys.argv)
+        #print('argc =',argc)
+        if argc<4:
+                print('Usage:\tmergePDF.py output.pdf input1.pdf input2.pdf ...')
+                sys.exit(1)
+        for fpdf in sys.argv[2:argc]:
+                if not op.isfile(fpdf):	
+                        print(fpdf,' does not exist... skipping')
+                        continue
+                f = open(fpdf, 'rb')
+                print('-->',fpdf)
+                pdfReader = PyPDF2.PdfFileReader(f)
+                print('pages:',pdfReader.numPages)
+                for pageNum in range(pdfReader.numPages):
+                        pageObj = pdfReader.getPage(pageNum)
+                        pdfWriter.addPage(pageObj)
 
-
-	pdfOutputFile = open(sys.argv[1], 'wb')
-	pdfWriter.write(pdfOutputFile)
-	pdfOutputFile.close()
+        pdfOutputFile = open(sys.argv[1], 'wb')
+        pdfWriter.write(pdfOutputFile)
+        pdfOutputFile.close()
 	
