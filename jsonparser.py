038d6da520398d3d50d2dc38b4cf3760227a2a13
a-capra <andrea.capra85@gmail.com>
2019-10-18 11:52:03 -0700

    clean-up json parsers

--- jsonparser.py
+++ jsonparser.py
@@ -8,11 +8,29 @@ from pprint import pprint
 if len(argv) == 2:
     dbname=argv[1]
 else:
-    print('Usage:\t %s odbname.json'%argv[0])
+    print('Usage:\t %s file_name.json'%argv[0])
     exit(1)
 
 with open(dbname) as f:
     data = json.load(f)
 
-pprint(data)
+#pprint(data)
+#print(data['Equipment']['TDC']['Common']['Frontend file name'])
+#print(data['Equipment']['BVlvtop']['Settings'])
+#print(data['Equipment']['BVlvbot']['Settings'])
+
+while True:
+    for i in data:
+        if i[0] == '/':
+            continue
+        print(i)
+    print('------------------------------')
+    s = input('Choose one: ')
+#    print(type(data))
+    if isinstance(data[s], dict):
+        data=data[s]
+    else:
+        print(data[s])
+        break
+
 
