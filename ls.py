8eef6992bd06221413c09b63bd9da51ded97b7f9
Andrea <andrea.capra85@gmail.com>
2020-05-05 12:14:06 -0700

    take directory as input

--- ls.py
+++ ls.py
@@ -2,6 +2,6 @@ import fnmatch
 import os
 import sys
 
-for file in os.listdir('.'):
-    if fnmatch.fnmatch(file, sys.argv[1]):
+for file in os.listdir(sys.argv[1]):
+    if fnmatch.fnmatch(file, sys.argv[2]):
         print(os.path.abspath(file))
