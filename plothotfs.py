c9b3165b89daf6404c1b96d1a36833a33016b626
a-capra <andrea.capra85@gmail.com>
2023-10-26 14:55:42 -0700

    fix pandas: NotImplementedError: file structure not supported

--- plothotfs.py
+++ plothotfs.py
@@ -2,24 +2,34 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
+from os import environ
+from pathlib import Path
+from sys import exit
 
 def san(s):
     return float(s.strip('ms'))
 
-df=pd.read_table('/home/agdaq/hotfs.log',sep=' ',names=('date','time','data','hist'),
-                 parse_dates={'Datetime':[0,1]})
-df['data']=df['data'].apply(san)
-df['hist']=df['hist'].apply(san)
+fin = Path(f"{environ['HOME']}/hotfs.log")
+#fin = Path("./hotfs.log")
+if fin.is_file():
+    print(f"Reading {fin.as_posix()}")
+else:
+    print(f"{fin.as_posix()} does not exists")
+    exit()
+    
+df = pd.read_table(f'{fin.as_posix()}',delim_whitespace=True,names=('date','time','data','hist'),parse_dates={'Datetime':[0,1]},date_format='%d-%m-%Y %H:%M:%S')
+df['data'] = df['data'].apply(san)
+df['hist'] = df['hist'].apply(san)
 
 
-ax1=df.plot(x='Datetime',y=['data','hist'],yerr=3.,label=['data','history'],fmt='o',figsize=(16,11))
+ax1 = df.plot(x='Datetime',y=['data','hist'],yerr=3.,label=['data','history'],fmt='o',figsize=(16,11))
 ax1.set(xlabel="", ylabel="Runtime [ms]")
 ax1.grid(axis='y', linestyle='--', linewidth=2)
 ax1.xaxis.label.set_size(20)
 ax1.yaxis.label.set_size(20)
 ax1.tick_params(labelsize='x-large')
 
-fig=plt.gcf()
+fig = plt.gcf()
 fig.tight_layout()
 plt.savefig('ls_time_series.pdf')
 plt.show()
